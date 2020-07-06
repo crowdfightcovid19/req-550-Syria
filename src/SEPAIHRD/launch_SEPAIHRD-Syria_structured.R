070f660f8365eb43ef327ad7e5bec95bb50e21b9
apascual <alberto.pascual.garcia@gmail.com>
2020-07-06 15:38:33 +0200

    added some lines to control the test code

--- src/SEPAIHRD/launch_SEPAIHRD-Syria_structured.R
+++ src/SEPAIHRD/launch_SEPAIHRD-Syria_structured.R
@@ -10,16 +10,22 @@
 #    SEPAIHRD-Syria_structured.R. These parameters are related to input and output files
 #    some simulation options and computational variables such as the number of realizations.
 #    Details about the simulations are described in the header of SEPAIHRD-Syria_structured.R
-#    and scripts for running multiple times are named multiple_launch_SEPAIHRD_$experiment.R
+#    and a script to run simulations under different scenarios is launch_multiple_SEPAIHRD-Syria_structured.R
 # usage = Fix the parameters below and source.
 #
 # Fix parameters ----------------------
 # .... These are the parameters related to input and output of files and computational
 # .... options. Epidemiological parameters are hardcoded in the file "input_parameters_$model.R"
 # .... and are not expected to be changed.
+
+rm(list=ls())
 ###### START EDITING
-# --- Structure of directories and labelling 
+
+# --- Options used for testing mode only
 fake=0 # fix to 1 if you are working with test data 
+test_sim=1 # fix to 1 to avoid generating output directories and files (debug purposes)
+
+# --- Structure of directories and labelling 
 descr="null_model_shield" # A string describing the model, input data should be created in a directory with that name in /data, outputs will be located there
 class.infected="age2_no_comorbid_orange" # string with the name of the class in which the first infection is detected
 
@@ -30,7 +36,6 @@ Nrand=10 # number of realizations of parameters
 
 
 # --- Model type
-CompModel="SEPAIHRD" # Only "SEPAIHRD" implemented 
 isolation=1 # if hospitalized leaves the camp =1, stays in the camp = 0.
 isoThr=2000 # If isolation=1, maximum capacity of H people isolation, the difference H-isoThr becomes infectious
 hospitalized2=1 # if hospitalized2 = 0, all hospitalized will recover, if = 1 all will die.
@@ -45,6 +50,8 @@ keywordB="green" # keyword to identify the second population class affected by T
 Nfull=2 # Number of simulations whose results will be fully reported (1 to Nrand)
 
 ######### STOP EDITING
+
+CompModel="SEPAIHRD" # Only "SEPAIHRD" implemented, but this string is required
 
 this.dir=strsplit(rstudioapi::getActiveDocumentContext()$path, "/src/")[[1]][1] # don't edit, just comment it if problems...
 dirCodeSpec=paste(this.dir,"/src/",CompModel,sep="") # Directory where code specific to this model
