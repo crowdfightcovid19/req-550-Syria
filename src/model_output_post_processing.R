7fb71163c1e02aea363ae690045d92283d51e1c5
jordan-klein <jdkny1@gmail.com>
2020-07-08 13:05:55 +0100

    Post-processed files of experiments A, B, and C

--- src/model_output_post_processing.R
+++ src/model_output_post_processing.R
@@ -5,13 +5,14 @@
 # email = jdklein@princeton.edu
 # date = 2nd July 2020
 # description = Imports .dat files from output directories for different models in real_models folder 
-#        (currently set to import from null_model_shield, model_1_1, and model_1_2). 
-#        Generates tidy dataframes for key variables of interest from imported summary .dat files
-#        (currently set to generate dataframes for total number of deaths and proportion dead 
-#        from "$DeathTolls_$Model_$options.dat" files and maximum number of infected cases from 
-#        "$MaxInfected_$Model_$options.dat" files). 
-#        Generates tables of summary statistics, pairwise t-test ouputs, test of normality, 
-#        and boxplots to compare models for selected variables of interest.
+#        Generates tidy dataframes for key variables of interest from imported summary .dat files. 
+#        Exports results to directory "data/real_models/results_post_processing"
+#        Exports results for the following variables to the denoted subdirectories:
+#         1. Fraction of final total population dead- "Final_fraction_dead"
+#         2. Fraction of final total population recovered- "Final_fraction_recovered"
+#         3. Time to peak number of infections in age3 with comorbidities- "Time_to_peak_infections_age3_comorbid"
+#         4. Time to steady state of susceptibles, maximum out of all pop classes- "Time_to_steady_state"
+#       **Currently performs post-processing for experiments A, B, and C
 # usage = script should be run within the folder "data/real_models". 
 #### Setup ####
 
@@ -34,11 +35,59 @@ get_output_directories <- function(directory) {
     .[grepl("/", .) & !grepl("figures", .)]
 }
 
-# Get directories
+## Get directories
+#*List names of directories to pull from*
 
-output_dirs <- lapply(c("null_model_shield", "model_1_1", "model_1_2"), get_output_directories) %>% unlist()
+# Experiment A
 
-### Import output data files
+exp_A_fateD <- lapply(c("null_model_mixed", "shield_cont10_age3_age2_20", "shield_cont2_age3_age2_20"), get_output_directories) %>% unlist() %>% 
+  .[grepl("IsolateNO", .) & grepl("FateD", .)]
+
+exp_A_fateR <- lapply(c("null_model_mixed", "shield_cont10_age3_age2_20", "shield_cont2_age3_age2_20"), get_output_directories) %>% unlist() %>% 
+  .[grepl("IsolateNO", .) & grepl("FateR", .)]
+
+# Experiment B
+
+exp_B_null <- get_output_directories("null_model_mixed") %>%
+  .[grepl("IsolateYES", .)]
+exp_B_2conts <- get_output_directories("shield_cont2_age3_age2_20") %>%
+  .[grepl("IsolateYES", .)]
+exp_B_10conts <- get_output_directories("shield_cont10_age3_age2_20") %>%
+  .[grepl("IsolateYES", .)]
+
+exp_b_isocap10 <- lapply(c("null_model_mixed", "shield_cont10_age3_age2_20", "shield_cont2_age3_age2_20"), get_output_directories) %>% unlist() %>% 
+  .[grepl("Limit10_", .)]
+exp_b_isocap25 <- lapply(c("null_model_mixed", "shield_cont10_age3_age2_20", "shield_cont2_age3_age2_20"), get_output_directories) %>% unlist() %>% 
+  .[grepl("Limit25_", .)]
+exp_b_isocap50 <- lapply(c("null_model_mixed", "shield_cont10_age3_age2_20", "shield_cont2_age3_age2_20"), get_output_directories) %>% unlist() %>% 
+  .[grepl("Limit50_", .)]
+exp_b_isocap100 <- lapply(c("null_model_mixed", "shield_cont10_age3_age2_20", "shield_cont2_age3_age2_20"), get_output_directories) %>% unlist() %>% 
+  .[grepl("Limit100_", .)]
+exp_b_isocap250 <- lapply(c("null_model_mixed", "shield_cont10_age3_age2_20", "shield_cont2_age3_age2_20"), get_output_directories) %>% unlist() %>% 
+  .[grepl("Limit250_", .)]
+exp_b_isocap500 <- lapply(c("null_model_mixed", "shield_cont10_age3_age2_20", "shield_cont2_age3_age2_20"), get_output_directories) %>% unlist() %>% 
+  .[grepl("Limit500_", .)]
+exp_b_isocap2000 <- lapply(c("null_model_mixed", "shield_cont10_age3_age2_20", "shield_cont2_age3_age2_20"), get_output_directories) %>% unlist() %>% 
+  .[grepl("Limit2000_", .)]
+
+# Experiment C
+
+exp_C <- lapply(c("shield_cont2_age3_age2_20", "shield_cont2_age3_age2_25", "shield_cont2_age3_age2_30", "shield_cont2_age3_age2", "shield_cont2_age3"), 
+                get_output_directories) %>% unlist() %>% 
+  .[grepl("IsolateNO", .)& grepl("FateD", .)]
+
+## List of all experiments
+
+exp_list <- list(exp_A_fateD, exp_A_fateR, 
+                 exp_B_null, exp_B_2conts, exp_B_10conts, 
+                 exp_b_isocap10, exp_b_isocap25, exp_b_isocap50, exp_b_isocap100, exp_b_isocap250, exp_b_isocap500, exp_b_isocap2000, 
+                 exp_C)
+names(exp_list) <- c("exp_A_fateD", "exp_A_fateR", 
+                "exp_B_null", "exp_B_2conts", "exp_B_10conts", 
+                "exp_b_isocap10", "exp_b_isocap25", "exp_b_isocap50", "exp_b_isocap100", "exp_b_isocap250", "exp_b_isocap500", "exp_b_isocap2000",
+                "exp_C")
+
+### Import output data files & generate tidy dataframes of key variables from models
 
 # Write function to import dat files
 
@@ -52,122 +101,75 @@ import_dat_files <- function(directories, variable) {
     })
 }
 
-# Import dat files for death tolls
+### Fraction of total population dead
 
-death_toll <- import_dat_files(output_dirs, "^DeathToll")
-names(death_toll) <- c(output_dirs)
+death_toll <- lapply(exp_list, function(x) {
+  data <- import_dat_files(x, "^NumFinalDeaths")
+  names(data) <- c(x)
+  
+  data_total <- lapply(data, rowSums) %>% 
+    bind_cols()
+  
+  data_tidy <- gather(data_total, "Model", "Fraction_Dead")
+  
+  data_tidy <- mutate(data_tidy, Fraction_Dead = Fraction_Dead/2000)
+  
+  return(data_tidy)
+})
 
-# Import dat files for max infected
+### Time to peak number of infections (elderly comorbid)
 
-max_infected <- import_dat_files(output_dirs, "^MaxInfected")
-names(max_infected) <- c(output_dirs)
+time_peak_infected <- lapply(exp_list, function(x) {
+  data <- import_dat_files(x, "^TimePeakInfected")
+  names(data) <- c(x)
+  
+  data_age3c <- sapply(data, function(x) {
+    x[, grep("age3_comorbid", names(x))]
+  }) %>% 
+    bind_cols()
+  
+  data_tidy <- gather(data_age3c, "Model", "Time_peak_infected")
+  
+  return(data_tidy)
+})
 
-### Generate tidy dataframes of key variables from models
+### Time to steady state susceptibles (last population class in each model to reach)
 
-## Total death toll
+time_steady_s <- lapply(exp_list, function(x) {
+  data <- import_dat_files(x, "^TimeS")
+  names(data) <- c(x)
+  
+  data_max_t <- lapply(data, function(x) {
+      apply(x, 1, max)
+    }) %>% 
+    bind_cols()
+  
+  data_tidy <- gather(data_max_t, "Model", "Time_to_steady_state")
+  
+  return(data_tidy)
+})
 
-death_toll_total <- lapply(death_toll, rowSums) %>% 
-  bind_cols() 
+### Fraction of total population recovered
 
-death_toll_total_tidy <- gather(death_toll_total, "model", "deaths")
+final_recover <- lapply(exp_list, function(x) {
+  data <- import_dat_files(x, "^NumFinalRecovered")
+  names(data) <- c(x)
+  
+  data_total <- lapply(data, rowSums) %>% 
+    bind_cols()
+  
+  data_tidy <- gather(data_total, "Model", "Fraction_Recovered")
+  
+  data_tidy <- mutate(data_tidy, Fraction_Recovered = Fraction_Recovered/2000)
+  
+  return(data_tidy)
+})
 
-## Proportion of total population dead
+#### Export files ####
 
-# Get total population in each model
+setwd("results_post_processing")
 
-total_pop <- names(death_toll_total) %>% 
-  str_extract("(?<=PopSize)\\d+") %>% 
-  as.numeric()
-
-# Compute proportion dead
-
-prop_total_dead <- mapply("/", death_toll_total, total_pop) %>% 
-  as.data.frame()
-
-prop_total_dead_tidy <- gather(prop_total_dead, "model", "prop_dead")
-
-## Max infected in entire pop
-
-max_infected_total <- lapply(max_infected, rowSums) %>% 
-  bind_cols()
-
-max_infected_total_tidy <- gather(max_infected_total, "model", "max_infected")
-
-#### Data analysis ####
-
-### Generate plots
-
-# Total death toll
-
-ggplot(death_toll_total_tidy, aes(model, deaths)) + geom_boxplot() + theme(axis.text.x = element_text(angle = 90))
-
-# Proportion dead
-
-ggplot(prop_total_dead_tidy, aes(model, prop_dead)) + geom_boxplot() + theme(axis.text.x = element_text(angle = 90))
-
-# Max infected
-
-ggplot(max_infected_total_tidy, aes(model, max_infected)) + geom_boxplot() + theme(axis.text.x = element_text(angle = 90))
-
-### Statistical analysis of results
-
-## Total death toll
-
-# Summary stats
-
-death_toll_sumstats <- death_toll_total_tidy %>% 
-  group_by(model) %>% 
-  get_summary_stats(deaths, type = "mean_sd")
-
-# Pairwise t-tests
-
-death_toll_ttest <- death_toll_total_tidy %>% 
-  pairwise_t_test(deaths ~ model, p.adjust.method = "BH")
-
-# See if normally distributed
-
-fitdist(death_toll_total_tidy$deaths, "norm") %>% 
-  plot()
-
-descdist(death_toll_total_tidy$deaths)
-
-## Proportion dead
-
-# Summary stats
-
-prop_dead_sumstats <- prop_total_dead_tidy %>% 
-  group_by(model) %>% 
-  get_summary_stats(prop_dead, type = "mean_sd")
-
-# Pairwise t-tests
-
-prop_dead_ttest <- prop_total_dead_tidy %>% 
-  pairwise_t_test(prop_dead ~ model, p.adjust.method = "BH")
-
-# See if normally distributed
-
-fitdist(prop_total_dead_tidy$prop_dead, "norm") %>% 
-  plot()
-
-descdist(prop_total_dead_tidy$prop_dead)
-
-## Max infected
-
-# Summary stats
-
-max_infected_sumstats <- max_infected_total_tidy %>% 
-  group_by(model) %>% 
-  get_summary_stats(max_infected, type = "mean_sd")
-
-# Pairwise t-tests
-
-max_infected_ttest <- max_infected_total_tidy %>% 
-  pairwise_t_test(max_infected ~ model, p.adjust.method = "BH")
-
-# See if normally distributed
-
-fitdist(max_infected_total_tidy$max_infected, "norm") %>% 
-  plot()
-
-descdist(max_infected_total_tidy$max_infected)
-
+mapply(write.csv, death_toll, file=paste0("Final_fraction_dead/", names(death_toll), ".csv"))
+mapply(write.csv, final_recover, file=paste0("Final_fraction_recovered/", names(final_recover), ".csv"))
+mapply(write.csv, time_peak_infected, file=paste0("Time_to_peak_infections_age3_comorbid/", names(time_peak_infected), ".csv"))
+mapply(write.csv, time_steady_s, file=paste0("Time_to_steady_state/", names(time_steady_s), ".csv"))
