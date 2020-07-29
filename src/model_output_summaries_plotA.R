831a3274ab5d0cadf9e6705803eb48099ed06b5c
apascual <alberto.pascual.garcia@gmail.com>
2020-07-29 02:33:29 +0200

    rename plot scripts and input files to follow new experiments names

--- src/model_output_summaries_plotA.R
+++ src/model_output_summaries_plotA.R
@@ -1,9 +1,9 @@
 # Plots for model_output_summaries.R
-# Experiment H -- self-distancing
+# Experiment A -- shielding
 
 # --- Now you can create some plots
 setwd(dirCode)
-fileExp="input_parameters_multiple_output_summaries_H.csv" # A file with the comparisons you want to do
+fileExp="input_parameters_multiple_output_summaries_A.csv" # A file with the comparisons you want to do
 params.df=read.table(file=fileExp,header = TRUE,sep=",") 
 Ncomp=dim(params.df)[1]
 
@@ -13,21 +13,21 @@ df.out=df.output # just preventive as.data.frame(df.output,stringsAsFactors = FA
 # --- Subset table
 df.sub = extract_subtable_output_summaries(Ncomp,df.out,params.df)
 
-# --- Determine if your analysis require any transformation, e.g. reodering levels  
+# --- Reorder levels if needed
 setwd(dirPlotOut)
-levels(df.sub$self)
-idx.self=c(3,1,2) # reorder levels
-df.sub$self=factor(df.sub$self,levels(df.sub$self)[idx.self])
-levels(df.sub$self)
+levels(df.sub$contacts)
+# idx.self=c(1,1,2) # not needed
+#df.sub$self=factor(df.sub$self,levels(df.sub$self)[idx.self])
+#levels(df.sub$self)
 
 
-#  Self distancing for the null model, fraction of deaths
-varX="self"
+#  Shielding for the null model, fraction of deaths
+varX="contacts"
 varY="NumFinalDeaths.mean" #"`P.outbrk.E`"
 errY="NumFinalDeaths.stderr"
-xlabel="Reduction personal contact (%)"
+xlabel="Number of contacts per week/individual"
 ylabel="Fraction of population dying"
-titlelabel="Self-distancing"
+titlelabel="Shielding"
 Npop=2000
 
 filePlotOut=paste(varX,"Vs",varY,".pdf",sep="")
@@ -51,7 +51,7 @@ gg=ggplot(data = df.sub) +  # assign columns to axes and groups
         legend.title = element_text(size=28),
         title=element_text(size=16))+ # Increase fonts size
   #scale_colour_manual(values=col_qual)+
-  scale_x_discrete( labels=c("0","20","50"))+
+  scale_x_discrete( labels=c("No limit","10","2"))+
   labs(title = titlelabel,
        subtitle = paste("Population. size = ",Npop))
 print(gg)
@@ -59,25 +59,37 @@ dev.off( )
 
 
 #  Self distancing for the null model, time to peak
-varX="self"
-varY="TimePeakInfected.mean" #"`P.outbrk.E`"
-errY="TimePeakInfected.stderr"
-xlabel="Reduction personal contact (%)"
+labX="contacts"
+labY="TimePeakInfected"
+varX=df.sub[,"contacts"]
+varY1=df.sub[,"TimePeakInfected.mean.E"] #"`P.outbrk.E`"
+errY1=df.sub[,"TimePeakInfected.stderr.E"]
+varY2=df.sub[,"TimePeakInfected.mean.S"] #"`P.outbrk.E`"
+errY2=df.sub[,"TimePeakInfected.stderr.S"]
+xlabel="Number of contacts per week/individual"
 ylabel="Time to peak of infected (days)"
-titlelabel="Self-distancing"
+titlelabel="Shielding"
 Npop=2000
 
-filePlotOut=paste(varX,"Vs",varY,".pdf",sep="")
+filePlotOut=paste(labX,"Vs",labY,".pdf",sep="")
 dataY=df.sub[,varY]
-errX=df.sub[,varX]
-errMin=df.sub[,varY]-df.sub[,errY]
-errMax=df.sub[,varY]+df.sub[,errY]
+errX=varX
+errMin1=varY1-errY1
+errMax1=varY1+errY1
+errMin2=varY2-errY2
+errMax2=varY2+errY2
 
 pdf(file=filePlotOut,width=9,height = 7)
 gg=ggplot(data = df.sub) +  # assign columns to axes and groups
-  geom_point(aes_string(x = varX, y = varY),size=2)+
-  geom_line(aes_string(x = varX, y = varY,group=1),color="red")+
-  geom_errorbar(aes(x=errX,ymin=errMin, ymax=errMax), width=.1)+
+  geom_point(aes(x = varX, y = varY1, colour="exposed"),size=2)+
+  geom_line(aes(x = varX, y = varY1, colour = "exposed", group=1))+
+  geom_point(aes(x = varX, y = varY2,colour ="shielded"),size=2)+
+  geom_line(aes(x = varX, y = varY2, colour = "shielded", group=1),show.legend = TRUE)+
+  geom_errorbar(aes(x=errX,ymin=errMin1, ymax=errMax1,colour="exposed"), width=.1)+
+  geom_errorbar(aes(x=errX,ymin=errMin2, ymax=errMax2,colour="shielded"), width=.1)+
+  scale_color_manual("",
+                     breaks=c("exposed","shielded"),
+                     values =c("orange","green"))+
   #geom_bar(stat="identity") +                  # represent data as lines
   xlab(xlabel)+           # add label for x axis
   ylab(ylabel) +     # add label for y axis
@@ -88,8 +100,111 @@ gg=ggplot(data = df.sub) +  # assign columns to axes and groups
         legend.title = element_text(size=28),
         title=element_text(size=16))+ # Increase fonts size
   #scale_colour_manual(values=col_qual)+
-  scale_x_discrete( labels=c("0","20","50"))+
+  scale_x_discrete( labels=c("No limit","10","2"))+
   labs(title = titlelabel,
-       subtitle = paste("Population. size = ",Npop))
+       subtitle = paste("Population. size = ",Npop,sep=""),
+       colour="Pop. class")
+       
+print(gg)
+dev.off( )
+
+
+#  Outbreak probability
+labX="contacts"
+labY="P.outbrk"
+varX=df.sub[,"contacts"]
+varY1=df.sub[,"P.outbrk.E"] #"`P.outbrk.E`"
+#errY1=df.sub[,"P.outbrk.stderr.E"]
+varY2=df.sub[,"P.outbrk.S"] #"`P.outbrk.E`"
+#browser()
+#errY2=df.sub[,"P.outbrk.stderr.S"]
+xlabel="Number of contacts per week/individual"
+ylabel="Probability outbreak"
+titlelabel="Shielding"
+Npop=2000
+
+filePlotOut=paste(labX,"Vs",labY,".pdf",sep="")
+
+# errMin1=varY1-errY1
+# errMax1=varY1+errY1
+# errMin2=varY2-errY2
+# errMax2=varY2+errY2
+
+pdf(file=filePlotOut,width=9,height = 7)
+gg=ggplot(data = df.sub) +  # assign columns to axes and groups
+  geom_point(aes(x = varX, y = varY1, colour="exposed"),size=2)+
+  geom_line(aes(x = varX, y = varY1, colour = "exposed", group=1))+
+  geom_point(aes(x = varX, y = varY2,colour ="shielded"),size=2)+
+  geom_line(aes(x = varX, y = varY2, colour = "shielded", group=1),show.legend = TRUE)+
+  #geom_errorbar(aes(x=errX,ymin=errMin1, ymax=errMax1,colour="exposed"), width=.1)+
+  #geom_errorbar(aes(x=errX,ymin=errMin2, ymax=errMax2,colour="shielded"), width=.1)+
+  scale_color_manual("",
+                     breaks=c("exposed","shielded"),
+                     values =c("orange","green"))+
+  #geom_bar(stat="identity") +                  # represent data as lines
+  xlab(xlabel)+           # add label for x axis
+  ylab(ylabel) +     # add label for y axis
+  theme_bw()+
+  theme(axis.title = element_text(size=22),
+        axis.text = element_text(size=20), # ,angle=90, hjust = 1,vjust=0.5),
+        legend.text = element_text(size=22),
+        legend.title = element_text(size=28),
+        title=element_text(size=16))+ # Increase fonts size
+  #scale_colour_manual(values=col_qual)+
+  scale_x_discrete( labels=c("No limit","10","2"))+
+  labs(title = titlelabel,
+       subtitle = paste("Population. size = ",Npop,sep=""),
+       colour="Pop. class")
+
+print(gg)
+dev.off( )
+
+
+#  Death fractions differentiating classes
+labX="contacts"
+labY="NumFinalDeaths.mean.byClass"
+varX=df.sub[,"contacts"]
+varY1=df.sub[,"NumFinalDeaths.mean.E"] #"`P.outbrk.E`"
+errY1=df.sub[,"NumFinalDeaths.stderr.E"]
+varY2=df.sub[,"NumFinalDeaths.mean.S"] #"`P.outbrk.E`"
+errY2=df.sub[,"NumFinalDeaths.stderr.S"]
+xlabel="Number of contacts per week/individual"
+ylabel="Fraction population dying"
+titlelabel="Shielding"
+Npop=2000
+
+filePlotOut=paste(labX,"Vs",labY,".pdf",sep="")
+errX=varX
+errMin1=varY1-errY1
+errMax1=varY1+errY1
+errMin2=varY2-errY2
+errMax2=varY2+errY2
+
+pdf(file=filePlotOut,width=9,height = 7)
+gg=ggplot(data = df.sub) +  # assign columns to axes and groups
+  geom_point(aes(x = varX, y = varY1, colour="exposed"),size=2)+
+  geom_line(aes(x = varX, y = varY1, colour = "exposed", group=1))+
+  geom_point(aes(x = varX, y = varY2,colour ="shielded"),size=2)+
+  geom_line(aes(x = varX, y = varY2, colour = "shielded", group=1),show.legend = TRUE)+
+  geom_errorbar(aes(x=errX,ymin=errMin1, ymax=errMax1,colour="exposed"), width=.1)+
+  geom_errorbar(aes(x=errX,ymin=errMin2, ymax=errMax2,colour="shielded"), width=.1)+
+  scale_color_manual("",
+                     breaks=c("exposed","shielded"),
+                     values =c("orange","green"))+
+  #geom_bar(stat="identity") +                  # represent data as lines
+  xlab(xlabel)+           # add label for x axis
+  ylab(ylabel) +     # add label for y axis
+  theme_bw()+
+  theme(axis.title = element_text(size=22),
+        axis.text = element_text(size=20), # ,angle=90, hjust = 1,vjust=0.5),
+        legend.text = element_text(size=22),
+        legend.title = element_text(size=28),
+        title=element_text(size=16))+ # Increase fonts size
+  #scale_colour_manual(values=col_qual)+
+  scale_x_discrete( labels=c("No limit","10","2"))+
+  labs(title = titlelabel,
+       subtitle = paste("Population. size = ",Npop,sep=""),
+       colour="Pop. class")
+
 print(gg)
 dev.off( )
--- src/model_output_summaries_plotB.R
+++ src/model_output_summaries_plotB.R
@@ -1,9 +1,9 @@
 # Plots for model_output_summaries.R
-# Experiment A -- shielding
+# Experiment B --- self-isolation in tents -- null model
 
 # --- Now you can create some plots
 setwd(dirCode)
-fileExp="input_parameters_multiple_output_summaries_A.csv" # A file with the comparisons you want to do
+fileExp="input_parameters_multiple_output_summaries_B.csv" # A file with the comparisons you want to do
 params.df=read.table(file=fileExp,header = TRUE,sep=",") 
 Ncomp=dim(params.df)[1]
 
@@ -15,19 +15,19 @@ df.sub = extract_subtable_output_summaries(Ncomp,df.out,params.df)
 
 # --- Reorder levels if needed
 setwd(dirPlotOut)
-levels(df.sub$contacts)
-# idx.self=c(1,1,2) # not needed
-#df.sub$self=factor(df.sub$self,levels(df.sub$self)[idx.self])
-#levels(df.sub$self)
+levels(df.sub$Limit)
+# idx.self=c(3,1,2) # reorder levels
+# df.sub$self=factor(df.sub$self,levels(df.sub$self)[idx.self])
+# levels(df.sub$self)
 
 
-#  Shielding for the null model, fraction of deaths
-varX="contacts"
+#  Self distancing for the null model, fraction of deaths
+varX="Limit"
 varY="NumFinalDeaths.mean" #"`P.outbrk.E`"
 errY="NumFinalDeaths.stderr"
-xlabel="Number of contacts per week/individual"
+xlabel="Isolation capacity (individuals)"
 ylabel="Fraction of population dying"
-titlelabel="Shielding"
+titlelabel="Isolation"
 Npop=2000
 
 filePlotOut=paste(varX,"Vs",varY,".pdf",sep="")
@@ -51,45 +51,34 @@ gg=ggplot(data = df.sub) +  # assign columns to axes and groups
         legend.title = element_text(size=28),
         title=element_text(size=16))+ # Increase fonts size
   #scale_colour_manual(values=col_qual)+
-  scale_x_discrete( labels=c("No limit","10","2"))+
+  scale_x_discrete( labels=c("0","10","25","50","100"))+
   labs(title = titlelabel,
        subtitle = paste("Population. size = ",Npop))
 print(gg)
 dev.off( )
 
 
-#  Self distancing for the null model, time to peak
-labX="contacts"
-labY="TimePeakInfected"
-varX=df.sub[,"contacts"]
-varY1=df.sub[,"TimePeakInfected.mean.E"] #"`P.outbrk.E`"
-errY1=df.sub[,"TimePeakInfected.stderr.E"]
-varY2=df.sub[,"TimePeakInfected.mean.S"] #"`P.outbrk.E`"
-errY2=df.sub[,"TimePeakInfected.stderr.S"]
-xlabel="Number of contacts per week/individual"
+#  time to peak
+varX="Limit"
+varY="TimePeakInfected.mean" #"`P.outbrk.E`"
+errY="TimePeakInfected.stderr"
+xlabel="Isolation capacity (individuals)"
 ylabel="Time to peak of infected (days)"
-titlelabel="Shielding"
+titlelabel="Isolation"
+
 Npop=2000
 
-filePlotOut=paste(labX,"Vs",labY,".pdf",sep="")
+filePlotOut=paste(varX,"Vs",varY,".pdf",sep="")
 dataY=df.sub[,varY]
-errX=varX
-errMin1=varY1-errY1
-errMax1=varY1+errY1
-errMin2=varY2-errY2
-errMax2=varY2+errY2
+errX=df.sub[,varX]
+errMin=df.sub[,varY]-df.sub[,errY]
+errMax=df.sub[,varY]+df.sub[,errY]
 
 pdf(file=filePlotOut,width=9,height = 7)
 gg=ggplot(data = df.sub) +  # assign columns to axes and groups
-  geom_point(aes(x = varX, y = varY1, colour="exposed"),size=2)+
-  geom_line(aes(x = varX, y = varY1, colour = "exposed", group=1))+
-  geom_point(aes(x = varX, y = varY2,colour ="shielded"),size=2)+
-  geom_line(aes(x = varX, y = varY2, colour = "shielded", group=1),show.legend = TRUE)+
-  geom_errorbar(aes(x=errX,ymin=errMin1, ymax=errMax1,colour="exposed"), width=.1)+
-  geom_errorbar(aes(x=errX,ymin=errMin2, ymax=errMax2,colour="shielded"), width=.1)+
-  scale_color_manual("",
-                     breaks=c("exposed","shielded"),
-                     values =c("orange","green"))+
+  geom_point(aes_string(x = varX, y = varY),size=2)+
+  geom_line(aes_string(x = varX, y = varY,group=1),color="red")+
+  geom_errorbar(aes(x=errX,ymin=errMin, ymax=errMax), width=.1)+
   #geom_bar(stat="identity") +                  # represent data as lines
   xlab(xlabel)+           # add label for x axis
   ylab(ylabel) +     # add label for y axis
@@ -100,47 +89,34 @@ gg=ggplot(data = df.sub) +  # assign columns to axes and groups
         legend.title = element_text(size=28),
         title=element_text(size=16))+ # Increase fonts size
   #scale_colour_manual(values=col_qual)+
-  scale_x_discrete( labels=c("No limit","10","2"))+
+  scale_x_discrete( labels=c("0","10","25","50","100"))+
   labs(title = titlelabel,
-       subtitle = paste("Population. size = ",Npop,sep=""),
-       colour="Pop. class")
-       
+       subtitle = paste("Population. size = ",Npop))
 print(gg)
 dev.off( )
 
 
-#  Outbreak probability
-labX="contacts"
-labY="P.outbrk"
-varX=df.sub[,"contacts"]
-varY1=df.sub[,"P.outbrk.E"] #"`P.outbrk.E`"
-#errY1=df.sub[,"P.outbrk.stderr.E"]
-varY2=df.sub[,"P.outbrk.S"] #"`P.outbrk.E`"
-#browser()
-#errY2=df.sub[,"P.outbrk.stderr.S"]
-xlabel="Number of contacts per week/individual"
+#  Probability outbreak
+varX="Limit"
+varY="P.outbrk.E" #"`P.outbrk.E`"
+#errY="TimePeakInfected.stderr"
+xlabel="Isolation capacity (individuals)"
 ylabel="Probability outbreak"
-titlelabel="Shielding"
+titlelabel="Isolation"
+
 Npop=2000
 
-filePlotOut=paste(labX,"Vs",labY,".pdf",sep="")
-
-# errMin1=varY1-errY1
-# errMax1=varY1+errY1
-# errMin2=varY2-errY2
-# errMax2=varY2+errY2
+filePlotOut=paste(varX,"Vs",varY,".pdf",sep="")
+dataY=df.sub[,varY]
+errX=df.sub[,varX]
+errMin=df.sub[,varY]-df.sub[,errY]
+errMax=df.sub[,varY]+df.sub[,errY]
 
 pdf(file=filePlotOut,width=9,height = 7)
 gg=ggplot(data = df.sub) +  # assign columns to axes and groups
-  geom_point(aes(x = varX, y = varY1, colour="exposed"),size=2)+
-  geom_line(aes(x = varX, y = varY1, colour = "exposed", group=1))+
-  geom_point(aes(x = varX, y = varY2,colour ="shielded"),size=2)+
-  geom_line(aes(x = varX, y = varY2, colour = "shielded", group=1),show.legend = TRUE)+
-  #geom_errorbar(aes(x=errX,ymin=errMin1, ymax=errMax1,colour="exposed"), width=.1)+
-  #geom_errorbar(aes(x=errX,ymin=errMin2, ymax=errMax2,colour="shielded"), width=.1)+
-  scale_color_manual("",
-                     breaks=c("exposed","shielded"),
-                     values =c("orange","green"))+
+  geom_point(aes_string(x = varX, y = varY),size=2)+
+  geom_line(aes_string(x = varX, y = varY,group=1),color="red")+
+  #geom_errorbar(aes(x=errX,ymin=errMin, ymax=errMax), width=.1)+
   #geom_bar(stat="identity") +                  # represent data as lines
   xlab(xlabel)+           # add label for x axis
   ylab(ylabel) +     # add label for y axis
@@ -151,60 +127,8 @@ gg=ggplot(data = df.sub) +  # assign columns to axes and groups
         legend.title = element_text(size=28),
         title=element_text(size=16))+ # Increase fonts size
   #scale_colour_manual(values=col_qual)+
-  scale_x_discrete( labels=c("No limit","10","2"))+
+  scale_x_discrete( labels=c("0","10","25","50","100"))+
   labs(title = titlelabel,
-       subtitle = paste("Population. size = ",Npop,sep=""),
-       colour="Pop. class")
-
-print(gg)
-dev.off( )
-
-
-#  Death fractions differentiating classes
-labX="contacts"
-labY="NumFinalDeaths.mean.byClass"
-varX=df.sub[,"contacts"]
-varY1=df.sub[,"NumFinalDeaths.mean.E"] #"`P.outbrk.E`"
-errY1=df.sub[,"NumFinalDeaths.stderr.E"]
-varY2=df.sub[,"NumFinalDeaths.mean.S"] #"`P.outbrk.E`"
-errY2=df.sub[,"NumFinalDeaths.stderr.S"]
-xlabel="Number of contacts per week/individual"
-ylabel="Fraction population dying"
-titlelabel="Shielding"
-Npop=2000
-
-filePlotOut=paste(labX,"Vs",labY,".pdf",sep="")
-errX=varX
-errMin1=varY1-errY1
-errMax1=varY1+errY1
-errMin2=varY2-errY2
-errMax2=varY2+errY2
-
-pdf(file=filePlotOut,width=9,height = 7)
-gg=ggplot(data = df.sub) +  # assign columns to axes and groups
-  geom_point(aes(x = varX, y = varY1, colour="exposed"),size=2)+
-  geom_line(aes(x = varX, y = varY1, colour = "exposed", group=1))+
-  geom_point(aes(x = varX, y = varY2,colour ="shielded"),size=2)+
-  geom_line(aes(x = varX, y = varY2, colour = "shielded", group=1),show.legend = TRUE)+
-  geom_errorbar(aes(x=errX,ymin=errMin1, ymax=errMax1,colour="exposed"), width=.1)+
-  geom_errorbar(aes(x=errX,ymin=errMin2, ymax=errMax2,colour="shielded"), width=.1)+
-  scale_color_manual("",
-                     breaks=c("exposed","shielded"),
-                     values =c("orange","green"))+
-  #geom_bar(stat="identity") +                  # represent data as lines
-  xlab(xlabel)+           # add label for x axis
-  ylab(ylabel) +     # add label for y axis
-  theme_bw()+
-  theme(axis.title = element_text(size=22),
-        axis.text = element_text(size=20), # ,angle=90, hjust = 1,vjust=0.5),
-        legend.text = element_text(size=22),
-        legend.title = element_text(size=28),
-        title=element_text(size=16))+ # Increase fonts size
-  #scale_colour_manual(values=col_qual)+
-  scale_x_discrete( labels=c("No limit","10","2"))+
-  labs(title = titlelabel,
-       subtitle = paste("Population. size = ",Npop,sep=""),
-       colour="Pop. class")
-
+       subtitle = paste("Population. size = ",Npop))
 print(gg)
 dev.off( )
