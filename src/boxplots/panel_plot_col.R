92b000e30aec93dd8420243437e554c69197b61d
Eduard Campillo-Funollet <e.campillo-funollet@sussex.ac.uk>
2020-08-20 13:27:20 +0100

    boxplot without median line

--- src/boxplots/panel_plot_col.R
+++ src/boxplots/panel_plot_col.R
@@ -14,12 +14,12 @@ currentDir <- getwd()
 
 source("plot_routines.R")
 
-setwd("/home/ecam/workbench/req-550-Syria")
+setwd("/home/ec365/workbench/req-550-Syria")
 
 #ptitle <- c("boxplot","boxmean","boxmedian","vio","viomean","viomedian","ribbonsd","ribbonmedian","ribbonse")
 #fplot.list <- c(do_box_plot,do_box_plot_mean,do_box_plot_median,do_vio_plot,do_vio_plot_mean,do_vio_plot_median,do_ribbon_sd,do_ribbon_quartile,do_ribbon_se)
 
-ptitle <- c("boxmeandot")
+ptitle <- c("boxmeandot2")
 fplot.list <- c(do_box_plot_mean_dot)
 
 
