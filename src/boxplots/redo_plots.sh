f3a63aa5e3d6f13a02583649480f876d6ee9db42
Eduard Campillo-Funollet <e.campillo-funollet@sussex.ac.uk>
2021-02-26 21:37:24 +0000

    Figures for new simulations

---
+++ src/boxplots/redo_plots.sh
@@ -0,0 +1,9 @@
+cd ..
+Rscript model_output_summaries.R
+cd boxplots
+Rscript generate_table_results.R
+Rscript extend_table_results.R
+Rscript panel_plot_Fig2.R
+Rscript panel_plot_Fig3.R
+Rscript supp_figures.R
+Rscript safety_age_groups.R
