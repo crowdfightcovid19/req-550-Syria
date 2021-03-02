3b752d02584b70c95d450b28a82241315a6eaf0e
Eduard Campillo-Funollet <e.campillo-funollet@sussex.ac.uk>
2021-03-02 12:27:14 +0000

    New figure: maxexposed vs timepeak

---
+++ src/boxplots/MaxExposed_vs_TimePeak.R
@@ -0,0 +1,98 @@
+#***************************************
+#panel_plot.R
+#***************************************
+#
+#
+#author = Eduard Campillo-Funollet
+#email = e.campillo-funollet@sussex.ac.uk
+#date = 15th Aug 2020
+#description = Produces panel plot  
+#usage = Edit setwd to base dir and run
+
+
+currentDir <- getwd()
+
+source("plot_routines.R")
+
+library(dplyr)
+
+setwd("/home/ecam/workbench/req-550-Syria")
+
+#ptitle <- c("boxplot","boxmean","boxmedian","vio","viomean","viomedian","ribbonsd","ribbonmedian","ribbonse")
+#fplot.list <- c(do_box_plot,do_box_plot_mean,do_box_plot_median,do_vio_plot,do_vio_plot_mean,do_vio_plot_median,do_ribbon_sd,do_ribbon_quartile,do_ribbon_se)
+
+ptitle <- c("boxmeandot")
+fplot.list <- c(do_box_plot_mean_dot)
+
+axis.text.size = 24
+axis.title.size = 27
+legend.title.size = 27
+legend.text.size = 24
+axis.text.x.size = 24
+title.size = 35
+
+varPoutbreak <- "POutbreak"
+varFracDeath <- "FracFinalDeaths"
+varPeak <- "TimePeakSymptomatic"
+
+ytitPoutbreak <- "Probability of Outbreak"
+ytitFracDeath <- "Fraction of Population Dying"
+ytitPeak <- "Time to Peak Symptomatic (days)"
+
+#Directories
+baseDir <- getwd()
+codeDir <- paste(baseDir,"/src",sep="")
+dataDir <- paste(baseDir,"/data/real_models",sep="")
+outDir <- paste(baseDir,"/data/real_models/results_post_processing",sep="")
+outPlotDir <- paste(baseDir,"/manuscripts/main/figures/newFig",sep="")
+
+idDir="modSV" # this is a string contained in all the directories that should be processed
+fileIn=paste("extended_results_table_",idDir,".csv",sep="")
+
+df.all <- read.csv(paste(outDir,"/",fileIn,sep=""))
+
+#Extract relevant data from table
+setwd(codeDir)
+
+params.df <- read.table(file=paste(codeDir,"/","input_parameters_multiple_output_summaries_H.csv",sep=""),header = TRUE, sep=",")
+df.self <- extract_subtable_output_summaries(df.all,params.df)
+
+idx.self<- c(6,1,2,3,4,5)
+df.self$self<-factor(df.self$self,levels(df.self$self)[idx.self])
+
+df.self$FracMaxExposed <- df.self$FracMaxExposed / 100. #Fraction, rather than %.
+
+#Self distancing
+df <- df.self
+outFile = "MaxExposed_vs_TimePeak"
+varX = "TimePeakSymptomatic"
+varY = "FracMaxExposed"
+xlabel = "Time to peak symptomatic (days)"
+ylabel = "Max. fraction of population exposed"
+#scale_x_labels <- c("0","10","20","30","40","50")
+scale_color_labels <- c("0%","10%","20%","30%","40%","50%")
+group_name = "Reduction of contacts"
+
+gg <-ggplot(df,aes(x=TimePeakSymptomatic,y=FracMaxExposed))+
+     geom_point(size=3,aes(colour=self))+
+     scale_color_discrete(name=group_name,labels=scale_color_labels)+
+     xlab(xlabel)+
+     ylab(ylabel)+
+     theme( legend.text = element_text(size=legend.text.size),
+            legend.title = element_text(size=legend.title.size),
+            axis.text = element_text(size=axis.text.size),
+            axis.title = element_text(size=axis.title.size),
+            panel.grid.major = element_line(size = 0.5, linetype = 'solid', colour = "lightgrey"),
+            panel.grid.minor = element_line(size = 0.25, linetype = 'solid', colour = "lightgrey"),
+            panel.background = element_rect(fill = "white", colour = "black", linetype = "solid"))
+
+    
+
+    
+
+setwd(outPlotDir)
+pdf(file=paste(outFile,".pdf",sep=""),width=30,height = 20,title=outFile)
+print(gg)
+dev.off( )
+
+setwd(currentDir) #Let's finish where we started.
