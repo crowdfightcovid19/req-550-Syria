43eabb94a0a4ad81cc6f42d4b8882493a03c811f
Eduard Campillo-Funollet <e.campillo-funollet@sussex.ac.uk>
2021-03-09 22:28:46 +0000

    Figures with more self-isolation values

--- src/boxplots/redo_plots.sh
+++ src/boxplots/redo_plots.sh
@@ -1,8 +1,8 @@
 #cd ..
 #Rscript model_output_summaries.R
 #cd boxplots
-#Rscript generate_table_results.R
-#Rscript extend_table_results.R
+Rscript generate_table_results.R
+Rscript extend_table_results.R
 Rscript panel_plot_Fig2.R
 Rscript panel_plot_Fig3.R
 Rscript supp_figures.R
