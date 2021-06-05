2253d3417f5ef868f44ff7a629db79f9d74147aa
Eduard Campillo-Funollet <e.campillo-funollet@sussex.ac.uk>
2021-06-05 09:55:53 +0100

    Fixed FigS5. Checked all figures

--- src/boxplots/panel_plot_Fig2.R
+++ src/boxplots/panel_plot_Fig2.R
@@ -90,6 +90,13 @@ for(i in 1:length(ptitle)){
     scale_x_labels <- c("No limit","10","2")
     scale_fill_labels <- c("Total","Exposed Zone", "Safety Zone")
     group_name = "Group"
+    cat("\n")
+    cat(varX)
+    cat("\n")
+    cat(levels(df.shield$contacts))
+    cat("\n")
+    cat(scale_x_labels)
+    cat("\n")
 
     #gg.Poutbreak.shield <- do_line_plot(df.shield,varPoutbreak,varX,xlabel,ytitPoutbreak,"mean",scale_x_labels,scale_fill_labels,group_name,nolegend=FALSE)
     #gg.FracDeath.shield <- fplot(df.shield,varFracDeath,varX,xlabel,ytitFracDeath,scale_x_labels,scale_fill_labels,group_name,nolegend=TRUE)
@@ -111,6 +118,15 @@ for(i in 1:length(ptitle)){
     scale_x_labels <- c("0","10","25","50","100","250","500","2000")
     scale_fill_labels <- c("Total")
     group_name = "Group"
+    
+    cat("\n")
+    cat(varX)
+    cat("\n")
+    cat(levels(df.iso$Limit))
+    cat("\n")
+    cat(scale_x_labels)
+    cat("\n")
+
 
     #gg.Poutbreak.iso <- do_line_plot(df.iso,varPoutbreak,varX,xlabel,ytitPoutbreak,"mean",scale_x_labels,scale_fill_labels,group_name,nolegend=TRUE)
     #gg.FracDeath.iso <- fplot(df.iso,varFracDeath,varX,xlabel,ytitFracDeath,scale_x_labels,scale_fill_labels,group_name,nolegend=TRUE)
@@ -133,6 +149,14 @@ for(i in 1:length(ptitle)){
     scale_x_labels <- c("0","10","20","30","40","50","60","70","80","90")
     scale_fill_labels <- c("Total")
     group_name = "Group"
+    cat("\n")
+    cat(varX)
+    cat("\n")
+    cat(levels(df.self$self))
+    cat("\n")
+    cat(scale_x_labels)
+    cat("\n")
+
 
     #gg.Poutbreak.self <- do_line_plot(df.self,varPoutbreak,varX,xlabel,ytitPoutbreak,"mean",scale_x_labels,scale_fill_labels,group_name,nolegend=TRUE)
     #gg.FracDeath.self <- fplot(df.self,varFracDeath,varX,xlabel,ytitFracDeath,scale_x_labels,scale_fill_labels,group_name,nolegend=TRUE)
