d1af6133b6ff47ab6df35e25e4ce1b8e2c211a7a
apascual <alberto.pascual.garcia@gmail.com>
2021-06-22 21:00:48 +0200

    alternative implementations of self-isolation and results

---
+++ src/SEPAIHRD/make_transitionsV2.R
@@ -0,0 +1,58 @@
+# ****************************************
+# make_transitions.R
+# ****************************************
+# 
+# 
+# author = Alberto Pascual-García 
+# email = alberto.pascual.garcia@gmail.com 
+# date = 9th July 2020
+# description = This function creates the transitions for running the tau-leaping simulations.
+# usage = It is sourced within SEPAIHRD-Syria_structured.R
+
+make_transitions = function(class.names,var.names,hospitalized2){
+  require(adaptivetau)
+  
+  i=0
+  for(Ref in class.names){
+    i=i+1
+    S.vars=paste(Ref,"S",sep=".") # Create labels to handle variables and transitions
+    E.vars=paste(Ref,"E",sep=".")
+    P.vars=paste(Ref,"P",sep=".")
+    A.vars=paste(Ref,"A",sep=".")
+    IR.vars=paste(Ref,"IR",sep=".")
+    IH.vars=paste(Ref,"IH",sep=".")
+    ID.vars=paste(Ref,"ID",sep=".")
+    H.vars=paste(Ref,"H",sep=".")
+    R.vars=paste(Ref,"R",sep=".")
+    D.vars=paste(Ref,"D",sep=".")
+    if(hospitalized2=="D"){
+      DR.vars=paste(Ref,"D",sep=".")
+    }else{
+      DR.vars=paste(Ref,"R",sep=".")
+    }
+    A.tmp=cbind(rbind(S.vars,-1,E.vars,+1), # This matrix lists all the transitions for class Ref
+            rbind(E.vars,-1,P.vars,+1), 
+            rbind(P.vars,-1,A.vars,+1),
+            rbind(P.vars,-1,IR.vars,+1), 
+            rbind(P.vars,-1,IH.vars,+1),  
+            rbind(P.vars,-1,ID.vars,+1),  
+            rbind(A.vars,-1,R.vars,+1), 
+            rbind(IR.vars,-1,R.vars,+1),
+            rbind(IH.vars,-1,H.vars,+1),   
+            rbind(ID.vars,-1,D.vars,+1),   
+            rbind(H.vars,-1,DR.vars,+1))
+    if(i==1){
+      A=A.tmp
+    }else{
+      A=cbind(A,A.tmp)
+    }
+  }
+  
+  trans=ssa.maketrans(var.names,A) # Create the transitions list
+  
+  return(trans)
+}
+
+
+
+
